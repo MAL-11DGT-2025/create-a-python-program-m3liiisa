2d0b9b5f18701f54415168673244fc9f3bb5e1b5
m3liiisa <nethuli.fernando@marcellin.school.nz>
2025-03-04 10:51:55 +1300

    added an age statement

--- hello.py
+++ hello.py
@@ -3,5 +3,8 @@ print('Hello world')
 name = input('what is your name?')
 print(f'Kia ora {name}')
 
-age = input('what is your age?')
+age = int(input('what is your age?'))
 print(f'{name} your {age}???? thats pretty old') 
+
+if age > 25:
+    print('you oldyyy') 
