ee179038ee3ef953e914936c7a0cbba3247ab399
m3liiisa <nethuli.fernando@marcellin.school.nz>
2025-03-04 10:46:39 +1300

    age an name statement

--- hello.py
+++ hello.py
@@ -2,5 +2,6 @@ print('Hello world')
 
 name = input('what is your name?')
 print(f'Kia ora {name}')
+
 age = input('what is your age?')
-print(f'{name} did you say you were {age}? thats old') 
+print(f'{name} your {age}???? thats pretty old') 
