bb06c8e9f6d7bb52ab52c3e2df919585f2d57802
m3liiisa <nethuli.fernando@marcellin.school.nz>
2025-03-06 15:12:23 +1300

    Created a story function which uses user input

---
+++ story.py
@@ -0,0 +1,12 @@
+def tell_a_story(age, name, car, food, whanau, game):
+    print(f'Kia ora {name}, thats a very pretty name btw. Your {age}???? your an oldyyyy. Oooh {car} is a very cool car! i would like to drive one too. wdum??? {food} is really good!! And i totally agree with you, {whanau} is the worst! lets make fun of them together sometime! You play {game}?? slayyyyyy +181780 aura points.')
+
+
+age = input("How old are you: ")
+name = input("What is your name: ")
+car = input("What is your favourite car: ")
+food = input('What is the worst food: ')
+whanau = input('What is the worst whanau/house: ')
+game = input('What is your favourite game: ')
+
+tell_a_story(age, name, car, food, whanau, game)
