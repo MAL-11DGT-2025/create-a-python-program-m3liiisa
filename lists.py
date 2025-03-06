db42684cd050b57b26f7729db6b48af0d0a6c4df
m3liiisa <nethuli.fernando@marcellin.school.nz>
2025-03-07 12:54:38 +1300

    added for loops

---
+++ lists.py
@@ -0,0 +1,15 @@
+colours = ['red', 'green', 'blue', 'yellow', 'purple']
+print(colours[0]) #this prints the first colour red.
+print(colours[1])
+
+for i in colours:
+    #print(i)  #this prints the whole list
+
+print(colours[-1])  #this outputs the last colour in the list
+
+print(len(colours))  #this tells us how many colours are on the list
+
+for i in colours:
+    if i == 'yellow':
+        print('There is yellow') #this will only print if 'yellow' is on the list.
+
