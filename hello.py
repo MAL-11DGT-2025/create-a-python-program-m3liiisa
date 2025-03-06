0967c91cc381af52c8a4a61a3c1b8efb7b53a3c0
m3liiisa <nethuli.fernando@marcellin.school.nz>
2025-03-06 14:43:50 +1300

    Added code for age and commented all code

--- hello.py
+++ hello.py
@@ -1,13 +1,30 @@
-print('Hello world')
+#print('Hello world')
 
-name = input('what is your name?')
-print(f'Kia ora {name}')
+#name = input('what is your name?')
+#print(f'Kia ora {name}')
 
-age = int(input('what is your age?'))
-print(f'{name} your {age}???? thats pretty old') 
+#age = int(input('what is your age?'))
+#print(f'{name} your {age}???? thats pretty old') 
 
-if age > 25:
-    print('you oldyyy') 
+#if age > 25:
+ #   print('you oldyyy') 
 
-if age >= 13 and age <= 19:
-    print('Your a teen!!')
+#if age >= 13 and age <= 19:
+ #   print('Your a teen!!')
+
+#if age >= 13 or age <= 19:
+ #   print('Your might be a teenager!')
+
+#umbrella = False
+#sunny = True
+
+#if umbrella and sunny:
+ #   print("You think it's going to rain")
+#elif not umbrella and sunny:
+ #   print("You think you don't need an umbrella")
+#elif umbrella and not sunny:
+ #   print("It must be raining.")
+#elif not umbrella and not sunny:
+ #   print("you must be drenched.")
+
+
